844ffc6db7db910d1f109a9020c66c258477fc1d
SergeyChemkayev <chemkayev.sergey@gmail.com>
2019-02-12 18:47:34 +0300

    converter

--- app/src/main/java/com/example/diary/data/TasksDao.java
+++ app/src/main/java/com/example/diary/data/TasksDao.java
@@ -3,6 +3,7 @@ package com.example.diary.data;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.TypeConverters;
 
 import com.example.diary.entities.Task;
 
@@ -15,7 +16,7 @@ import io.reactivex.Flowable;
 public abstract class TasksDao {
 
     @Query("SELECT * FROM tasks WHERE time_stamp BETWEEN :from AND :to")
-    public abstract Flowable<List<Task>> findTasksBetweenDates(Date from, Date to);
+    public abstract Flowable<List<Task>> findTasksBetweenDates(@TypeConverters({DateConverter.class}) Date from, @TypeConverters({DateConverter.class}) Date to);
 
     @Insert
     public abstract void insertTask(Task task);
